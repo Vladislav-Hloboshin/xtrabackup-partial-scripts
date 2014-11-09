5e78225e1a044140e508a9f34e9d9266e505224c
Vladislav Hloboschin <hloboshin.vladislav@gmail.com>
2014-11-09 03:22:28 +0300

    added set global innodb_import_table_from_xtrabackup

--- restore.sh
+++ restore.sh
@@ -17,6 +17,7 @@ DEFINE_string 'user'		'xtrabackup'		''	'u'
 FLAGS "$@" || exit 1
 eval set -- "${FLAGS_ARGV}"
 
+mysql -u"${FLAGS_user}" --execute "set global innodb_import_table_from_xtrabackup=1;" || exit 1
 mysql -u"${FLAGS_user}" --execute "drop database if exists ${FLAGS_database}; create database ${FLAGS_database};" || exit 1
 
 TDIR=`mktemp -d`
